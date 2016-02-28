0e0e7171a80f2ee58fce506e4c07ceac7754348b
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2016-02-28 11:48:55 +0700

    Finish maps marker, fix here and there

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 angular.module('ourWedding', ['uiGmapgoogle-maps'])
 	.controller('WeddingCtrl', function($scope) {
-		$scope.map = { center: { latitude: -6.2454024, longitude: 107.0577429 }, zoom: 15 };
+		$scope.map = { 
+			center: {
+			 latitude: -6.245403, 
+			 longitude: 107.059932 
+			}, 
+			zoom: 16
+		};
+
+		$scope.marker = {
+			id: 0,
+			coords: {
+			  latitude: -6.243412699363186, 
+			  longitude: 107.06103064119816
+			}
+		}
 	});
