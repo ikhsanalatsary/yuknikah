5c5f5a10f6e01e2e961e54494100ec3623a7dfb7
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2016-03-01 07:52:03 +0700

    Add quotes

--- js/main.js
+++ js/main.js
@@ -15,4 +15,13 @@ angular.module('ourWedding', ['uiGmapgoogle-maps'])
 			  longitude: 107.06103064119816
 			}
 		}
+
+		$scope.quotes = [
+			{"id":1, "quote":"Akan tetapi sehebat apapun kita merencanakan sesuatu. Tetap rencana Allah adalah sebaik-baiknya rancangan."},
+			{"id":2, "quote":"Aku tak berharap kesempurnaanmu. Kerana aku ingin melengkapinya dengan kekuranganku."},
+			{"id":3, "quote":"Yang paling sempurna percaya dalam iman adalah orang yang karakter terbaik dan yang paling baik kepada istrinya."},
+			{"id":4, "quote":"Selama dalam penantian akan jodoh, perbanyaklah senyum. selama dalam kesendirian perbanyaklah sujud."},
+			{"id":5, "quote":"Pantaskan dirimu untuk jodohmu. Karena saat ini ia pun sedang memantaskan dirinya untukmu."},
+			{"id":6, "quote":"jodohmu adalah pemberian paling manis dan terbaik yg diberikan Allah padamu."}
+		];
 	});
