4289b7719f667e86f5310fa6558c7976ebc8ec32
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2016-03-05 09:44:02 +0700

    Add single click & double click social media, add info, fix here & there

--- js/main.js
+++ js/main.js
@@ -1,5 +1,65 @@
+'use strict';
+
 angular.module('ourWedding', ['uiGmapgoogle-maps', 'matchMedia'])
 	.controller('WeddingCtrl', function($scope, $location, $anchorScroll, screenSize, $window, $timeout) {
+		var urlTw = {
+			pipit: 'https://twitter.com/fitripipitana',
+			ikhsan: 'https://twitter.com/abdfattahikhsan'
+		};
+
+		var urlFb = {
+			pipit: 'https://www.facebook.com/fitri.ana.716',
+			ikhsan: 'https://www.facebook.com/ikhsan.alatsary'
+		};
+
+		var urlGplus = {
+			pipit: 'https://plus.google.com/113383305409625413541',
+			ikhsan: 'https://plus.google.com/+abdulfattahikhsanalatsary'
+		};
+
+		var urlInsta = {
+			pipit: 'https://www.instagram.com/pipitana/',
+			ikhsan: 'https://www.instagram.com/ikhsan_alatsary/'
+		};
+
+		function twPipit() {
+			openUrl(urlTw.pipit);
+		}
+
+		function twIkhsan() {
+			openUrl(urlTw.ikhsan);
+		}
+
+		function fbPipit() {
+			openUrl(urlFb.pipit);
+		}
+
+		function fbIkhsan() {
+			openUrl(urlFb.ikhsan);
+		}
+
+		function gplusIkhsan() {
+			openUrl(urlGplus.ikhsan);
+		}
+
+		function gplusPipit() {
+			openUrl(urlGplus.pipit);
+		}
+
+		function instaIkhsan() {
+			openUrl(urlInsta.ikhsan);
+		}
+
+		function instaPipit() {
+			openUrl(urlInsta.pipit);
+		}
+
+		function openUrl(url) {
+			var openUrl = $window.open(url, '_blank');
+			openUrl.focus();
+		}
+
+
 		$window.onload = function() {
 			var element = document.getElementById("loading");
 			$timeout(function() {
@@ -25,7 +85,7 @@ angular.module('ourWedding', ['uiGmapgoogle-maps', 'matchMedia'])
 			  latitude: -6.243412699363186, 
 			  longitude: 107.06103064119816
 			}
-		}
+		};
 
 		$scope.quotes = [
 			{"id":1, "quote":"Akan tetapi sehebat apapun kita merencanakan sesuatu. Tetap rencana Allah adalah sebaik-baiknya rancangan."},
@@ -60,4 +120,68 @@ angular.module('ourWedding', ['uiGmapgoogle-maps', 'matchMedia'])
 		$scope.mobile = screenSize.on('xs, sm', function(match){
 		    $scope.mobile = match;
 		});
-	});
+		$scope.twIkhsan = twIkhsan;
+		$scope.twPipit = twPipit;
+		$scope.fbIkhsan = fbIkhsan;
+		$scope.fbPipit = fbPipit;
+		$scope.gplusPipit = gplusPipit;
+		$scope.gplusIkhsan = gplusIkhsan;
+		$scope.instaPipit = instaPipit;
+		$scope.instaIkhsan = instaIkhsan;
+	})
+	.directive('ngSglclick', ['$parse', function($parse) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attr) {
+              var fn = $parse(attr['ngSglclick']);
+              var delay = 300, clicks = 0, timer = null;
+              element.on('click', function (event) {
+                clicks++;  //count clicks
+                if(clicks === 1) {
+                  timer = setTimeout(function() {
+                    scope.$apply(function () {
+                        fn(scope, { $event: event });
+                    }); 
+                    clicks = 0;             //after action performed, reset counter
+                  }, delay);
+                  } else {
+                    clearTimeout(timer);    //prevent single-click action
+                    clicks = 0;             //after action performed, reset counter
+                  }
+              });
+            }
+        };
+    }])
+    .directive('iosDblclick',
+        function () {
+
+            const DblClickInterval = 300; //milliseconds
+
+            var firstClickTime;
+            var waitingSecondClick = false;
+
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    element.bind('click', function (e) {
+
+                        if (!waitingSecondClick) {
+                            firstClickTime = (new Date()).getTime();
+                            waitingSecondClick = true;
+
+                            setTimeout(function () {
+                                waitingSecondClick = false;
+                            }, DblClickInterval);
+                        }
+                        else {
+                            waitingSecondClick = false;
+
+                            var time = (new Date()).getTime();
+                            if (time - firstClickTime < DblClickInterval) {
+                                scope.$apply(attrs.iosDblclick);
+                            }
+                        }
+                    });
+                }
+            };
+        });
