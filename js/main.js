12eb6453359ec183c55fb0a3990ca32f278bf010
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2016-03-07 00:01:46 +0700

    Finishing, add favicon, refactor & clean code

--- js/main.js
+++ js/main.js
@@ -1,134 +1,7 @@
 'use strict';
 
+//**ANGULAR**//
 angular.module('ourWedding', ['uiGmapgoogle-maps', 'matchMedia'])
-	.controller('WeddingCtrl', function($scope, $location, $anchorScroll, screenSize, $window, $timeout) {
-		var urlTw = {
-			pipit: 'https://twitter.com/fitripipitana',
-			ikhsan: 'https://twitter.com/abdfattahikhsan'
-		};
-
-		var urlFb = {
-			pipit: 'https://www.facebook.com/fitri.ana.716',
-			ikhsan: 'https://www.facebook.com/ikhsan.alatsary'
-		};
-
-		var urlGplus = {
-			pipit: 'https://plus.google.com/113383305409625413541',
-			ikhsan: 'https://plus.google.com/+abdulfattahikhsanalatsary'
-		};
-
-		var urlInsta = {
-			pipit: 'https://www.instagram.com/pipitana/',
-			ikhsan: 'https://www.instagram.com/ikhsan_alatsary/'
-		};
-
-		function twPipit() {
-			openUrl(urlTw.pipit);
-		}
-
-		function twIkhsan() {
-			openUrl(urlTw.ikhsan);
-		}
-
-		function fbPipit() {
-			openUrl(urlFb.pipit);
-		}
-
-		function fbIkhsan() {
-			openUrl(urlFb.ikhsan);
-		}
-
-		function gplusIkhsan() {
-			openUrl(urlGplus.ikhsan);
-		}
-
-		function gplusPipit() {
-			openUrl(urlGplus.pipit);
-		}
-
-		function instaIkhsan() {
-			openUrl(urlInsta.ikhsan);
-		}
-
-		function instaPipit() {
-			openUrl(urlInsta.pipit);
-		}
-
-		function openUrl(url) {
-			var openUrl = $window.open(url, '_blank');
-			openUrl.focus();
-		}
-
-
-		$window.onload = function() {
-			var element = document.getElementById("loading");
-			$timeout(function() {
-				element.style.display = "none";
-			}, 3000);
-		}
-
-		$scope.map = { 
-			center: {
-			 latitude: -6.245403, 
-			 longitude: 107.059932 
-			}, 
-			zoom: 16,
-			options: {
-				scrollwheel: false,
-				draggable: isMobile()
-			}
-		};
-
-		$scope.marker = {
-			id: 0,
-			coords: {
-			  latitude: -6.243412699363186, 
-			  longitude: 107.06103064119816
-			}
-		};
-
-		$scope.quotes = [
-			{"id":1, "quote":"Akan tetapi sehebat apapun kita merencanakan sesuatu. Tetap rencana Allah adalah sebaik-baiknya rancangan."},
-			{"id":2, "quote":"Aku tak berharap kesempurnaanmu. Kerana aku ingin melengkapinya dengan kekuranganku."},
-			{"id":3, "quote":"Yang paling sempurna percaya dalam iman adalah orang yang karakter terbaik dan yang paling baik kepada istrinya."},
-			{"id":4, "quote":"Selama dalam penantian akan jodoh, perbanyaklah senyum. selama dalam kesendirian perbanyaklah sujud."},
-			{"id":5, "quote":"Pantaskan dirimu untuk jodohmu. Karena saat ini ia pun sedang memantaskan dirinya untukmu."},
-			{"id":6, "quote":"jodohmu adalah pemberian paling manis dan terbaik yg diberikan Allah padamu."}
-		];
-
-		function isMobile() {
-			if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-				return false;
-			}
-			return true
-		}
-		function scrollTo(id) {
-			$location.hash(id);
-			$anchorScroll();
-		}
-
-		function isActive(id) {
-			return $location.hash() === id;
-		}
-
-		$scope.onload = $window.onload;
-		$scope.scrollTo = scrollTo;
-		$scope.isActive = isActive;
-		$scope.desktop = screenSize.on('md, lg', function(match){
-		    $scope.desktop = match;
-		});
-		$scope.mobile = screenSize.on('xs, sm', function(match){
-		    $scope.mobile = match;
-		});
-		$scope.twIkhsan = twIkhsan;
-		$scope.twPipit = twPipit;
-		$scope.fbIkhsan = fbIkhsan;
-		$scope.fbPipit = fbPipit;
-		$scope.gplusPipit = gplusPipit;
-		$scope.gplusIkhsan = gplusIkhsan;
-		$scope.instaPipit = instaPipit;
-		$scope.instaIkhsan = instaIkhsan;
-	})
 	.directive('ngSglclick', ['$parse', function($parse) {
         return {
             restrict: 'A',
@@ -184,4 +57,145 @@ angular.module('ourWedding', ['uiGmapgoogle-maps', 'matchMedia'])
                     });
                 }
             };
-        });
+    	}
+    )
+	.controller('WeddingCtrl', ['$scope', '$location', '$anchorScroll', 'screenSize', '$window', '$timeout', 
+		function($scope, $location, $anchorScroll, screenSize, $window, $timeout) {
+
+			//**PRIVATE**//
+			var urlTw = {
+				pipit: 'https://twitter.com/fitripipitana',
+				ikhsan: 'https://twitter.com/abdfattahikhsan'
+			};
+
+			var urlFb = {
+				pipit: 'https://www.facebook.com/fitri.ana.716',
+				ikhsan: 'https://www.facebook.com/ikhsan.alatsary'
+			};
+
+			var urlGplus = {
+				pipit: 'https://plus.google.com/113383305409625413541',
+				ikhsan: 'https://plus.google.com/+abdulfattahikhsanalatsary'
+			};
+
+			var urlInsta = {
+				pipit: 'https://www.instagram.com/pipitana/',
+				ikhsan: 'https://www.instagram.com/ikhsan_alatsary/'
+			};
+
+			function twPipit() {
+				openUrl(urlTw.pipit);
+			}
+
+			function twIkhsan() {
+				openUrl(urlTw.ikhsan);
+			}
+
+			function fbPipit() {
+				openUrl(urlFb.pipit);
+			}
+
+			function fbIkhsan() {
+				openUrl(urlFb.ikhsan);
+			}
+
+			function gplusIkhsan() {
+				openUrl(urlGplus.ikhsan);
+			}
+
+			function gplusPipit() {
+				openUrl(urlGplus.pipit);
+			}
+
+			function instaIkhsan() {
+				openUrl(urlInsta.ikhsan);
+			}
+
+			function instaPipit() {
+				openUrl(urlInsta.pipit);
+			}
+
+			function openUrl(url) {
+				var openUrl = $window.open(url, '_blank');
+				openUrl.focus();
+			}
+
+
+			$window.onload = function() {
+				var element = document.getElementById("loading");
+				$timeout(function() {
+					element.style.display = "none";
+				}, 3000);
+			}
+
+			function isMobile() {
+				if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+					return false;
+				}
+				return true;
+			}
+			function scrollTo(id) {
+				$location.hash(id);
+				$anchorScroll();
+			}
+
+			function isActive(id) {
+				return $location.hash() === id;
+			}
+
+			//**[PUBLIC] Export To view**//
+
+			$scope.map = { 
+				center: {
+				 latitude: -6.245403, 
+				 longitude: 107.059932 
+				}, 
+				zoom: 16,
+				options: {
+					scrollwheel: false,
+					draggable: isMobile()
+				}
+			};
+
+			$scope.marker = {
+				id: 0,
+				coords: {
+				  latitude: -6.243412699363186, 
+				  longitude: 107.06103064119816
+				}
+			};
+
+			$scope.quotes = [
+				{"id":1, "quote":"Akan tetapi sehebat apapun kita merencanakan sesuatu. Tetap rencana Allah adalah sebaik-baiknya rancangan."},
+				{"id":2, "quote":"Aku tak berharap kesempurnaanmu. Kerana aku ingin melengkapinya dengan kekuranganku."},
+				{"id":3, "quote":"Yang paling sempurna percaya dalam iman adalah orang yang karakter terbaik dan yang paling baik kepada istrinya."},
+				{"id":4, "quote":"Selama dalam penantian akan jodoh, perbanyaklah senyum. selama dalam kesendirian perbanyaklah sujud."},
+				{"id":5, "quote":"Pantaskan dirimu untuk jodohmu. Karena saat ini ia pun sedang memantaskan dirinya untukmu."},
+				{"id":6, "quote":"jodohmu adalah pemberian paling manis dan terbaik yg diberikan Allah padamu."}
+			];
+
+			$scope.onload = $window.onload;
+			$scope.scrollTo = scrollTo;
+			$scope.isActive = isActive;
+			$scope.desktop = screenSize.on('md, lg', function(match){
+			    $scope.desktop = match;
+			});
+			$scope.mobile = screenSize.on('xs, sm', function(match){
+			    $scope.mobile = match;
+			});
+			$scope.twIkhsan = twIkhsan;
+			$scope.twPipit = twPipit;
+			$scope.fbIkhsan = fbIkhsan;
+			$scope.fbPipit = fbPipit;
+			$scope.gplusPipit = gplusPipit;
+			$scope.gplusIkhsan = gplusIkhsan;
+			$scope.instaPipit = instaPipit;
+			$scope.instaIkhsan = instaIkhsan;
+		}
+	]);
+
+//**JQUERY**//
+$.material.init();
+$(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+});
